8c6e5cfaa6b6ab7cef4d4b53fb4a9162da97e0cf
Apurba <palapurba2004@gmail.com>
2025-03-05 11:17:46 +0530

    master and new voice buttons added

---
+++ app/(root)/voice_recognition/_components/VoiceActions.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Brain, Plus, HelpCircle } from "lucide-react";
+
+export const MasterButton = () => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Brain className="h-5 w-5" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Master Voice Commands</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
+
+export const AddNewButton = () => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Plus className="h-5 w-5" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Add New Command</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
+
+export const UnrecognisedButton = () => (
+  <TooltipProvider>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="outline" size="icon">
+          <HelpCircle className="h-5 w-5" />
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Unrecognised Commands</p>
+      </TooltipContent>
+    </Tooltip>
+  </TooltipProvider>
+);
--- app/(root)/voice_recognition/page.tsx
+++ app/(root)/voice_recognition/page.tsx
@@ -1,13 +1,24 @@
 import ConversationFallback from '@/components/shared/conversation/ConversationFallback'
 import ItemList from '@/components/shared/item-list/ItemList'
 import React from 'react'
+import { AddNewButton, MasterButton, UnrecognisedButton } from './_components/VoiceActions'
 
-type Props = {}
-
-const VoiceRecognitionPage = (props: Props) => {
+const VoiceRecognitionPage = () => {
   return (
     <>
-      <ItemList title='Voice Recognition' />
+      <ItemList 
+        title='Voice Recognition'
+        action={
+          <div className="flex items-center gap-2">
+            <MasterButton />
+            <AddNewButton />
+          </div>
+        }
+      >
+        <div className="mt-auto">
+          <UnrecognisedButton />
+        </div>
+      </ItemList>
       <ConversationFallback />
     </>
   )
